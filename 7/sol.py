620ced6ccd674fbf5eda3d3a76c217c84cf495d1
phase <phaseansible@gmail.com>
2023-12-06 22:15:30 -0800

    day 7, cleanup code

--- 7/sol.py
+++ 7/sol.py
@@ -19,7 +19,6 @@ def get_type(a: Counter):
         return 2
     elif sort[0] == 1 and sort[1] == 1:  # high
         return 1
-    print("invalid score")
 
 
 ord = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
@@ -33,24 +32,18 @@ def compare(a, b):
     # print(a)
     a_freq = Counter(a)
     b_freq = Counter(b)
-    print(a, a_freq)
 
-    # print(a, a_freq)
     a_type = get_type(a_freq)
     b_type = get_type(b_freq)
 
     if a_type == b_type:  # find first highest
         for i in range(5):
-            # print("a[i]", i, a[i])
             a_ord = ord.index(a[i])
             b_ord = ord.index(b[i])
             if a_ord != b_ord:
-                # return a_ord - b_ord
                 return b_ord - a_ord
     else:
         return a_type - b_type
-
-    print("invalid compare")
 
 
 with open('input.txt') as f:
@@ -59,8 +52,6 @@ with open('input.txt') as f:
     start = time.process_time_ns()
 
     hands = [(x.split()[0], int(x.split()[1])) for x in lines]
-    # print(compare(hands[0][0], hands[1][0]))
-
     print(hands)
 
     ordered = sorted(hands, key=functools.cmp_to_key(compare))
@@ -68,7 +59,7 @@ with open('input.txt') as f:
 
     score = 0
     for x in range(len(ordered)):
-        score += (x+1) * ordered[x][1]
+        score += (x + 1) * ordered[x][1]
     print(score)
 
     print((time.process_time_ns() - start) / 10e6, " ms")
