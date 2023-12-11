77970a8795ef6ff1da33018d629ebd3d5428f2aa
phase <phaseansible@gmail.com>
2023-12-11 13:01:58 -0800

    Was challenged to make a more optimal part 2. was fun

--- 11/sol2.py
+++ 11/sol2.py
@@ -1,75 +1,78 @@
+import timeit
+
 with open('input.txt') as f:
     lines = f.read().splitlines()
 
 
-def transpose(mat):
-    return [list(i) for i in zip(*mat)]
+def f():
+    def transpose(mat):
+        return [list(i) for i in zip(*mat)]
+
+    def check_empty(lst):
+        for char in lst:
+            if char == '#':
+                return False
+        return True
+
+    def remap(universe):
+        galax_y = []
+        galax_x = []
+        for y in range(len(universe)):
+            for x in range(len(universe[0])):
+                if universe[y][x] == '#':
+                    galax_y.append((y, y))  # orig value, expanded value
+                    galax_x.append((x, x))
+        return galax_y, galax_x
+
+    def find_empty_rows(orig):
+        empty_idx = []
+        for idx in range(len(orig)):
+            if check_empty(orig[idx]):
+                empty_idx.append(idx)
+        return empty_idx
+
+    gys, gxs = remap(lines)
+    # print("pre-expand galaxies: ")
+    # for i in range(len(gys)):
+    #     print(f"({gys[i][0]} {gxs[i][0]}) ", end='')
+    # print("\n------------")
+
+    empty_y = find_empty_rows(lines)
+    empty_x = find_empty_rows(transpose(lines))
+
+    # print("empty idxs")
+    # print("y", empty_y)
+    # print("x", empty_x)
+
+    multiplier = 1_000_000
+
+    for ey in empty_y:
+        for gy_idx in range(len(gys)):
+            curr_gy = gys[gy_idx]
+            if ey < curr_gy[0]:  # 0 is orig value, 01 is expanded value
+                gys[gy_idx] = (curr_gy[0], curr_gy[1] + multiplier - 1)  # this -01 cost me an hour.
+
+    # print("modified gys", gys)
+
+    for ex in empty_x:
+        for gx_idx in range(len(gxs)):
+            if ex < gxs[gx_idx][0]:  # 0 is orig value, 01 is expanded value
+                gxs[gx_idx] = (gxs[gx_idx][0], gxs[gx_idx][1] + multiplier - 1)
+
+    # print("modified gxs", gxs)
+
+    def get_dist(x1, y1, x2, y2):
+        return abs(x1 - x2) + abs(y1 - y2)
+
+    dist = 0
+    for gy1, gx1 in zip(gys, gxs):  # (origy, newy), (origx, newx)
+        for gy2, gx2 in zip(gys, gxs):
+            dist += get_dist(gx1[1], gy1[1], gx2[1], gy2[1])
+
+    # print(dist // 2)
+
+    return dist // 2
 
 
-def check_empty(lst):
-    for char in lst:
-        if char == '#':
-            return False
-    return True
-
-
-def remap(universe):
-    galax_y = []
-    galax_x = []
-    for y in range(len(universe)):
-        for x in range(len(universe[0])):
-            if universe[y][x] == '#':
-                galax_y.append((y, y))  # orig value, expanded value
-                galax_x.append((x, x))
-    return galax_y, galax_x
-
-
-def find_empty_rows(orig):
-    empty_idx = []
-    for idx in range(len(orig)):
-        if check_empty(orig[idx]):
-            empty_idx.append(idx)
-    return empty_idx
-
-
-gys, gxs = remap(lines)
-# print("pre-expand galaxies: ")
-# for i in range(len(gys)):
-#     print(f"({gys[i][0]} {gxs[i][0]}) ", end='')
-# print("\n------------")
-
-empty_y = find_empty_rows(lines)
-empty_x = find_empty_rows(transpose(lines))
-
-# print("empty idxs")
-# print("y", empty_y)
-# print("x", empty_x)
-
-multiplier = 1_000_000
-
-for ey in empty_y:
-    for gy_idx in range(len(gys)):
-        curr_gy = gys[gy_idx]
-        if ey < curr_gy[0]:  # 0 is orig value, 01 is expanded value
-            gys[gy_idx] = (curr_gy[0], curr_gy[1] + multiplier - 1)  # this -01 cost me an hour.
-
-# print("modified gys", gys)
-
-for ex in empty_x:
-    for gx_idx in range(len(gxs)):
-        if ex < gxs[gx_idx][0]:  # 0 is orig value, 01 is expanded value
-            gxs[gx_idx] = (gxs[gx_idx][0], gxs[gx_idx][1] + multiplier - 1)
-
-# print("modified gxs", gxs)
-
-
-def get_dist(x1, y1, x2, y2):
-    return abs(x1 - x2) + abs(y1 - y2)
-
-
-dist = 0
-for gy1, gx1 in zip(gys, gxs):  # (origy, newy), (origx, newx)
-    for gy2, gx2 in zip(gys, gxs):
-        dist += get_dist(gx1[1], gy1[1], gx2[1], gy2[1])
-
-print(dist // 2)
+x = timeit.timeit(lambda: f())
+print(x)
---
+++ 11/sol2_2.py
@@ -0,0 +1,58 @@
+import timeit
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+
+def f():
+    has_x = set()
+    has_y = set()
+    galaxies = []
+
+    for y in range(len(lines)):
+        for x in range(len(lines[0])):
+            if lines[y][x] == '#':
+                has_y.add(y)
+                has_x.add(x)
+                galaxies.append((y, x))
+
+    empty_y = [0] * len(lines)
+    empty_x = [0] * len(lines[0])
+
+    for y in range(len(lines)):
+        if y in has_y:
+            empty_y[y] = empty_y[y - 1] or 0
+        else:
+            empty_y[y] = empty_y[y - 1] + 1
+
+    for x in range(len(lines[0])):
+        if x in has_x:
+            empty_x[x] = empty_x[x - 1] or 0
+        else:
+            empty_x[x] = empty_x[x - 1] + 1
+
+    multiple = 1_000_000 - 1
+
+    def dist(g1, g2):
+        g1_y = g1[0] + empty_y[g1[0]] * multiple
+        g2_y = g2[0] + empty_y[g2[0]] * multiple
+
+        g1_x = g1[1] + empty_x[g1[1]] * multiple
+        g2_x = g2[1] + empty_x[g2[1]] * multiple
+
+        return abs(g2_y - g1_y) + abs(g2_x - g1_x)
+
+    dist_sum = 0
+    for g1 in range(len(galaxies)):
+        for g2 in range(g1 + 1, len(galaxies)):
+            dist_sum += dist(galaxies[g1], galaxies[g2])
+
+    # print(dist_sum)
+    return dist_sum
+
+
+f()
+
+iters = 1000
+x = timeit.timeit(lambda: f(), number=iters)
+print(f"{x} ms")
