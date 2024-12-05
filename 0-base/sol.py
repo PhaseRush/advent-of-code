bb833909606fbe572ab36e79ceb1c63518d83d07
phase <phaseansible@gmail.com>
2024-12-05 00:39:01 -0800

    Add toposort version for benchmarking

--- 0-base/sol.py
+++ 0-base/sol.py
@@ -2,15 +2,14 @@ import timeit
 
 with open('input.txt') as f:
     lines = f.read().splitlines()
+    print(lines)
 
 
 def f():
     pass
 
 
-print(lines)
-
 if __name__ == '__main__':
     iters = 1
     x = timeit.timeit(lambda: f(), number=iters)
-    print(f"{x: 0.2f} s")
+    print(f"{x / iters: 0.2f} s")
--- 2024/05/sol2_topo.py
+++ 2024/05/sol2_topo.py
@@ -1,6 +1,9 @@
 import collections
 import timeit
 
+import networkx
+from networkx import DiGraph
+
 with open('input.txt') as f:
     lines = f.read().splitlines()
     mid_line = lines.index("")
@@ -10,9 +13,11 @@ with open('input.txt') as f:
     print(conditions, printed)
 
     G_pre = collections.defaultdict(set)
+    edges = set()
     for con in conditions:
         [a, b] = con.split("|")
         G_pre[a].add(b)
+        edges.add((a, b))
 
 
 def check_row(nums):
@@ -23,26 +28,21 @@ def check_row(nums):
     return True
 
 
-def fix_row(nums):
-    while not check_row(nums):
-        for i in range(len(nums)):
-            for j in range(0, i):
-                if nums[i] not in G_pre[nums[j]]:
-                    if nums[j] in G_pre[nums[i]]:
-                        nums[i], nums[j] = nums[j], nums[i]
-    return int(nums[len(nums) // 2])
-
-
 def f():
     s = 0
+
     for row in printed:
         nums = row.split(",")
-        if not check_row(nums):
-            s += fix_row(nums)
+        graph = DiGraph((a, b) for a, b in edges if a in nums and b in nums)
+        sorted = list(networkx.topological_sort(graph))
+        if sorted != nums:
+            s += int(sorted[len(sorted) // 2])
+
+    # print(s)
     return s
 
 
 if __name__ == '__main__':
-    iters = 1000
+    iters = 1
     x = timeit.timeit(lambda: f(), number=iters)
-    print(f"{x: 0.2f} s")
+    print(f"{x / iters: 0.2f} s")
