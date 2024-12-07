da38cf9ab14cbcb3ca0d6711cd411591de2829ce
phase <phaseansible@gmail.com>
2024-12-07 13:33:41 -0800

    fun d7 optimization using math instead of strings

--- 2024/07/sol.py
+++ 2024/07/sol.py
@@ -12,6 +12,38 @@ with open('input.txt') as f:
     # print(eqs)
 
 
+def nbdigit(x):  # https://stackoverflow.com/a/61889467
+    if x >= 10000000000000000:  # 17 -
+        return len(str(x))
+    if x < 100000000:  # 1 - 8
+        if x < 10000:  # 1 - 4
+            if x < 100:
+                return (x >= 10) + 1
+            else:
+                return (x >= 1000) + 3
+        else:  # 5 - 8
+            if x < 1000000:
+                return (x >= 100000) + 5
+            else:
+                return (x >= 10000000) + 7
+    else:  # 9 - 16
+        if x < 1000000000000:  # 9 - 12
+            if x < 10000000000:
+                return (x >= 1000000000) + 9
+            else:
+                return (x >= 100000000000) + 11
+        else:  # 13 - 16
+            if x < 100000000000000:
+                return (x >= 10000000000000) + 13
+            else:
+                return (x >= 1000000000000000) + 15
+
+
+def concat(a, b):
+    a = a * 10**nbdigit(b)
+    return a + b
+
+
 def solve(t, nums):
     start = [nums[0]]
     next = []
@@ -24,8 +56,9 @@ def solve(t, nums):
             if candidate * i <= t:
                 next.append(candidate * i)
 
-            if int(str(candidate) + str(i)) <= t:
-                next.append(int(str(candidate) + str(i)))
+            c = concat(candidate, i)
+            if c <= t:
+                next.append(c)
         start = next
         next = []
     for n in start:
