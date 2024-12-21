51bc4bd51a265717580388d7a498f73b824be583
phase <phaseansible@gmail.com>
2024-12-20 23:23:41 -0800

    d15p2

--- 2024/15/sol.py
+++ 2024/15/sol.py
@@ -1,14 +1,41 @@
 import timeit
 
-with open('test.txt') as f:
+
+def p():
+    print()
+    for r in grid:
+        print(*r)
+
+
+with open('input.txt') as f:
     lines = f.read().split("\n\n")
     grid = lines[0]
-    grid = [list(row) for row in grid.split("\n")]
+    initial_grid = [list(row) for row in grid.split("\n")]
     Y = len(grid)
     X = len(grid[0])
 
     y = 0
     x = 0
+    grid = []
+    for row in range(len(initial_grid)):
+        if len(grid) <= row or row not in grid:
+            grid.append([])
+
+        for col in range(len(initial_grid[0])):
+            # print(initial_grid[row][col])
+            match initial_grid[row][col]:
+                case "#":
+                    grid[row].extend(["#", "#"])
+                case "O":
+                    grid[row].extend(["[", "]"])
+                case ".":
+                    grid[row].extend([".", "."])
+                case "@":
+                    grid[row].extend(["@", "."])
+
+    Y = len(grid)
+    X = len(grid[0])
+    p()
     for i in range(Y):
         for j in range(X):
             if grid[i][j] == "@":
@@ -16,7 +43,7 @@ with open('test.txt') as f:
                 x = j
                 break
 
-    moves = lines[1].replace("\n","")
+    moves = lines[1].replace("\n", "")
     print(moves, moves.find("\n"))
 
 dirs = {
@@ -26,30 +53,56 @@ dirs = {
     "v": (1, 0)
 }
 
-def p():
-    print()
-    for r in grid:
-        print(*r)
 
-def valid(y, x) -> bool:
+def valid(y, x):
     return 0 <= x < X and 0 <= y < Y and grid[y][x] != "#"
 
 
-def move(pos, dir):
-    y_0, x_0 = pos[0], pos[1]
-    y_1, x_1 = y_0 + dir[0], x_0 + dir[1]
+def test(y_0, x_0, dy, dx, visited):
+    if (y_0, x_0) in visited:
+        return True
+    visited.add((y_0, x_0))
+
+    nr, nc = y_0 + dy, x_0 + dx
+    match grid[nr][nc]:
+        case "#":
+            return False
+        case "[":
+            return test(nr, nc, dy, dx, visited) and \
+                test(nr, nc + 1, dy, dx, visited)
+        case "]":
+            return test(nr, nc, dy, dx, visited) and \
+                test(nr, nc - 1, dy, dx, visited)
+        case "O":
+            return test(nr, nc, dy, dx, visited)
+    return True
+
+
+def move(y_0, x_0, delta):
+    dy = delta[0]
+    dx = delta[1]
+    y_1, x_1 = y_0 + dy, x_0 + dx
 
     if not valid(y_1, x_1):
-        return pos
+        return y_0, x_0
 
-    boxes = []
-    while grid[y_1][x_1] == "O":
-        boxes.append((y_1, x_1))
-        y_1, x_1 = y_1 + dir[0], x_1 + dir[1]
+    if grid[y_1][x_1] in "[]":
+        visited = set()
 
-    if grid[y_1][x_1] == ".":  # end was not a wall
-        grid[y_1][x_1], grid[y_0 + dir[0]][x_0 + dir[1]] = grid[y_0 + dir[0]][x_0 + dir[1]], grid[y_1][x_1]
+        if not test(y_0, x_0, dy, dx, visited):
+            return y_0, x_0
 
+        while visited:
+            for y_test, x_test in visited.copy():
+                y_test_1, x_test_1 = y_test + dy, x_test + dx
+                if (y_test_1, x_test_1) not in visited:
+                    if grid[y_test_1][x_test_1] != "@" and grid[y_test][x_test] != "@":
+                        grid[y_test_1][x_test_1] = grid[y_test][x_test]
+                        grid[y_test][x_test] = "."
+
+                    visited.remove((y_test, x_test))
+
+    grid[y_0][x_0], grid[y_1][x_1] = grid[y_1][x_1], grid[y_0][x_0]
     return y_1, x_1
 
 
@@ -57,13 +110,13 @@ def f():
     y_0 = y
     x_0 = x
     for m in moves:
-        y_0, x_0 = move((y_0, x_0), dirs[m])
-        p()
+        y_0, x_0 = move(y_0, x_0, dirs[m])
+        # p()
 
     s = 0
     for i in range(Y):
         for j in range(X):
-            if grid[i][j] == "O":
+            if grid[i][j] == "[":
                 s += 100 * i + j
     print(s)
     return s
